8e4bb297084b3610e78d3b2d90e70120959944cc
Yuriy Zakharchuk <y.a.zakharchuk@gmail.com>
2020-10-13 00:09:38 +0300

    separate functions for sse/mmx evaluations

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <functional>
 #include <cassert> 
 #include <chrono>
+#include <tuple>
 
 extern "C" {
 #include <sys/mman.h>
@@ -12,119 +14,21 @@ extern "C" {
 }
 
 
-void process_memory(void *memptr, size_t elements_to_calculate) {
-#ifdef MMX
+
+std::tuple<uint64_t, int>
+process_memory(void *memptr, size_t elements_to_calculate) {
     volatile uint8_t max_difference { 0 };
+    uint32_t high, low;
     __asm__ __volatile__ (
-
-        "       movq    %1, %%r8         \n"
-        "       movq    (%%r8), %%mm0    \n"
-        "       movq    1(%%r8), %%mm1   \n"
-        "       movq    %%mm0, %%mm2     \n"
-        "       psubusb %%mm1, %%mm0     \n"
-        "       psubusb %%mm2, %%mm1     \n"
-        "       por     %%mm1, %%mm0     \n"
-
-        "       movq    8(%%r8), %%mm1   \n"
-        "       movq    9(%%r8), %%mm2   \n"
-        "       movq    %%mm1, %%mm3     \n"
-        "       psubusb %%mm2, %%mm1     \n"
-        "       psubusb %%mm3, %%mm2     \n"
-        "       por     %%mm2, %%mm1     \n"
-        "       pmaxub  %%mm1, %%mm0     \n"
-
-        "       movq    16(%%r8), %%mm1  \n"
-        "       movq    17(%%r8), %%mm2  \n"
-        "       movq    %%mm1, %%mm3     \n"
-        "       psubusb %%mm2, %%mm1     \n"
-        "       psubusb %%mm3, %%mm2     \n"
-        "       por     %%mm2, %%mm1     \n"
-        "       pmaxub  %%mm1, %%mm0     \n"
-
-        "       movq    %%mm0, %%rax     \n"
-        "       rol     $32, %%rax       \n"
-        "       movq    %%rax, %%mm1     \n"
-        "       pmaxub  %%mm1, %%mm0     \n"
-
-        "       movq    %%mm0, %%rax     \n"
-        "       rol     $8, %%rax        \n"
-        "       movq    %%rax, %%mm1     \n"
-        "       pmaxub  %%mm1, %%mm0     \n"
-
-        "       movq    %%mm0, %%rax     \n"
-        "       rol     $16, %%rax       \n"
-        "       movq    %%rax, %%mm1     \n"
-        "       pmaxub  %%mm1, %%mm0     \n"        
-
-        "       movq    %%mm0, %%rax     \n"
-        "       emms                     \n"
-        "       movb    %%al, %0         \n"
-
-        : "=r"(max_difference)
-        : "r"(memptr), "r"(elements_to_calculate)
-        : "%rax", "%r8", "%mm0", "%mm1", "%mm2"
+        "       rdtsc                       \n"
+        "       movl    %%edx, %0           \n"
+        "       movl    %%eax, %1           \n"
+        
+        : "=r"(high), "=r"(low)
+        :: "%rax", "%rbx", "%rcx", "%rdx"
     );
+    uint64_t ticks = ((uint64_t)high << 32) | low;
 
-    std::cout << "Result: " << static_cast<uint32_t>(max_difference)
-              << " Time needed: " 
-              << " ns" << std::endl;
-
-#elif SSE
-    volatile static float differences[4];
-    static constexpr uint8_t abs_mask[16]
-    {   
-        0xFF, 0xFF, 0xFF, 0x7F,
-        0xFF, 0xFF, 0xFF, 0x7F,
-        0xFF, 0xFF, 0xFF, 0x7F, 
-        0xFF, 0xFF, 0xFF, 0x7F 
-    };
-
-    __asm__ __volatile__ (        
-        "       movupd  (%2), %%xmm0        \n"
-        "       xorpd   %%xmm3, %%xmm3      \n"
-        
-        "       movq    %1, %%rax           \n"
-        "       xorq    %%rdx, %%rdx        \n"
-        "       movq    $4, %%rbx           \n"
-        "       div     %%rbx               \n"
-        "       cmpq    $0, %%rdx           \n"
-        "       jnz     loop                \n"
-        "       decq    %%rax               \n"
-        "loop:                              \n"
-        "       movups  (%0), %%xmm1        \n"
-        "       movups  4(%0), %%xmm2       \n"
-        "       subps   %%xmm2, %%xmm1      \n"
-        "       andps   %%xmm0, %%xmm1      \n"
-        "       maxps   %%xmm1, %%xmm3      \n"
-        "       addq    $16, %0             \n"
-        "       decq    %%rax               \n"
-        "       jnz     loop                \n"
-        
-        "       cmpq    $1, %%rdx           \n"
-        "       je      write_max           \n"
-        
-        "       subq    %%rdx, %0           \n"
-        "       movups  (%0), %%xmm1        \n"
-        "       movups  4(%0), %%xmm2       \n"
-        "       subps   %%xmm2, %%xmm1      \n"
-        "       andps   %%xmm0, %%xmm1      \n"
-        "       maxps   %%xmm1, %%xmm3      \n"
-
-        "write_max:                         \n"
-        "       movups  %%xmm3, (%3)        \n"
-
-        :
-        : "r"(memptr), "r"(elements_to_calculate), "r"(abs_mask), "r"(differences)
-        : "%rax", "%rbx", "%rdx", "%xmm0", "%xmm1", "%xmm2", "%xmm3"
-    );
-
-
-    std::cout << "Result: " << *std::max_element(std::begin(differences), std::end(differences))
-              << " Time needed: " 
-              << " ns" << std::endl;        
-
-#else
-    volatile uint8_t max_difference { 0 };
     __asm__ __volatile__ (
         "       movq    %1, %%r8         \n"
         "       movq    %2, %%r9         \n"
@@ -154,10 +58,163 @@ void process_memory(void *memptr, size_t elements_to_calculate) {
         : "%rax", "%rbx", "%rcx", "%rdx", "%r8", "%r9"
     );
 
-    std::cout << "Result: " << static_cast<uint32_t>(max_difference)
-            << " Time needed: " 
-            << " ns" << std::endl;
-#endif
+    __asm__ __volatile__ (
+        "       rdtsc                       \n"
+        "       movl    %%edx, %0           \n"
+        "       movl    %%eax, %1           \n"
+        : "=r"(high), "=r"(low)
+        :: "%rax", "%rbx", "%rcx", "%rdx"
+    );
+
+    return std::make_tuple(
+        (((uint64_t)high << 32) | low) - ticks,
+        static_cast<int>(max_difference)
+    );
+}
+
+
+
+std::tuple<uint64_t, int>
+process_memory_mmx(void *memptr, size_t elements_to_calculate) {
+    volatile static uint8_t differences[8];
+    uint32_t high, low;
+    __asm__ __volatile__ (
+        "       rdtsc                       \n"
+        "       movl    %%edx, %0           \n"
+        "       movl    %%eax, %1           \n"
+        
+        : "=r"(high), "=r"(low)
+        :: "%rax", "%rbx", "%rcx", "%rdx"
+    );
+    uint64_t ticks = ((uint64_t)high << 32) | low;
+    __asm__ __volatile__ (
+
+        "       movq    %0, %%r8         \n"
+        "       movq    (%%r8), %%mm0    \n"
+        "       movq    1(%%r8), %%mm1   \n"
+        "       movq    %%mm0, %%mm2     \n"
+        "       psubusb %%mm1, %%mm0     \n"
+        "       psubusb %%mm2, %%mm1     \n"
+        "       por     %%mm1, %%mm0     \n"
+
+        "       movq    8(%%r8), %%mm1   \n"
+        "       movq    9(%%r8), %%mm2   \n"
+        "       movq    %%mm1, %%mm3     \n"
+        "       psubusb %%mm2, %%mm1     \n"
+        "       psubusb %%mm3, %%mm2     \n"
+        "       por     %%mm2, %%mm1     \n"
+        "       pmaxub  %%mm1, %%mm0     \n"
+
+        "       movq    16(%%r8), %%mm1  \n"
+        "       movq    17(%%r8), %%mm2  \n"
+        "       movq    %%mm1, %%mm3     \n"
+        "       psubusb %%mm2, %%mm1     \n"
+        "       psubusb %%mm3, %%mm2     \n"
+        "       por     %%mm2, %%mm1     \n"
+        "       pmaxub  %%mm1, %%mm0     \n"
+        "dbg:                            \n"
+        "       movq    %%mm0, (%2)      \n"
+
+        :: "r"(memptr), "r"(elements_to_calculate), "r"(differences)
+        : "%rax", "%r8", "%mm0", "%mm1", "%mm2"
+    );
+
+    __asm__ __volatile__ (
+        "       rdtsc                       \n"
+        "       movl    %%edx, %0           \n"
+        "       movl    %%eax, %1           \n"
+        : "=r"(high), "=r"(low)
+        :: "%rax", "%rbx", "%rcx", "%rdx"
+    );
+
+    return std::make_tuple(
+        (((uint64_t)high << 32) | low) - ticks,
+        static_cast<int>(*std::max_element(std::begin(differences), std::end(differences)))
+    );
+}
+
+
+
+std::tuple<uint64_t, float>
+process_memory_sse(void *memptr, size_t elements_to_calculate) {
+    alignas(16) volatile static float differences[4];
+    alignas(16) static constexpr uint8_t abs_mask[16]
+    {   
+        0xFF, 0xFF, 0xFF, 0x7F,
+        0xFF, 0xFF, 0xFF, 0x7F,
+        0xFF, 0xFF, 0xFF, 0x7F, 
+        0xFF, 0xFF, 0xFF, 0x7F 
+    };
+
+    uint32_t high, low;
+    __asm__ __volatile__ (
+        "       rdtsc                       \n"
+        "       movl    %%edx, %0           \n"
+        "       movl    %%eax, %1           \n"
+        
+        : "=r"(high), "=r"(low)
+        :: "%rax", "%rbx", "%rcx", "%rdx"
+    );
+    uint64_t ticks = ((uint64_t)high << 32) | low;
+
+    __asm__ __volatile__ (        
+        "       movupd  (%2), %%xmm0        \n"
+        "       xorpd   %%xmm3, %%xmm3      \n"
+        
+        "       movq    %1, %%rax           \n"
+        "       xorq    %%rdx, %%rdx        \n"
+        "       movq    $4, %%rbx           \n"
+        "       div     %%rbx               \n"
+        "       cmpq    $0, %%rdx           \n"
+        "       jnz     loop_sse            \n"
+        "       decq    %%rax               \n"
+        
+        "loop_sse:                          \n"
+        "       movups  (%0), %%xmm1        \n"
+        "       movups  4(%0), %%xmm2       \n"
+        "       subps   %%xmm2, %%xmm1      \n"
+        "       andps   %%xmm0, %%xmm1      \n"
+        "       maxps   %%xmm1, %%xmm3      \n"
+        "       addq    $16, %0             \n"
+        "       decq    %%rax               \n"
+        "       jnz     loop_sse            \n"
+        
+        "       cmpq    $1, %%rdx           \n"
+        "       je      write_max           \n"
+        
+        "       subq    %%rdx, %0           \n"
+        "       movups  (%0), %%xmm1        \n"
+        "       movups  4(%0), %%xmm2       \n"
+        "       subps   %%xmm2, %%xmm1      \n"
+        "       andps   %%xmm0, %%xmm1      \n"
+        "       maxps   %%xmm1, %%xmm3      \n"
+
+        "write_max:                         \n"
+        "       movups  %%xmm3, (%3)        \n"
+
+        :: "r"(memptr), "r"(elements_to_calculate), "r"(abs_mask), "r"(differences)
+        : "%rax", "%rbx", "%rdx", "%xmm0", "%xmm1", "%xmm2", "%xmm3"
+    );
+
+    __asm__ __volatile__ (
+        "       rdtsc                       \n"
+        "       movl    %%edx, %0           \n"
+        "       movl    %%eax, %1           \n"
+        : "=r"(high), "=r"(low)
+        :: "%rax", "%rbx", "%rcx", "%rdx"
+    );
+
+    return std::make_tuple(
+        (((uint64_t)high << 32) | low) - ticks,
+        *std::max_element(std::begin(differences), std::end(differences))
+    );
+}
+
+template <typename T>
+void print_result(T func, void *mapped_ptr, size_t array_len) {
+    const auto current_evaluated {func(mapped_ptr, array_len)};
+    std::cout << std::get<1>(current_evaluated) << " "
+              << std::get<0>(current_evaluated) << std::endl;
 }
 
 
@@ -190,7 +247,14 @@ int main(int argc, char *argv[]) {
         ::close(file_descriptor);
     }
     else {
-        process_memory(mapped_ptr, array_len);
+        
+#ifdef MMX
+        print_result(process_memory_mmx, mapped_ptr, array_len); 
+#elif SSE
+        print_result(process_memory_sse, mapped_ptr, array_len);
+#else
+        print_result(process_memory, mapped_ptr, array_len);
+#endif //MMX       
         ::munmap(mapped_ptr, array_len);
         ::close(file_descriptor);
     }
--- lab_1/run.sh
+++ lab_1/run.sh
@@ -3,14 +3,14 @@
 rm -rf main_1 main_2 main_3 > /dev/null
 
 echo 'Pure:'
-g++ main.cpp -o main_1 -O0 -Wall -pedantic -g3 > /dev/null
+g++ main.cpp -o main_1 -O3 -Wall -pedantic -std=c++17 > /dev/null
 ./main_1 example
 
 echo 'MMX:'
-g++ main.cpp -o main_2 -O0 -Wall -pedantic -DMMX -g3 > /dev/null
+g++ main.cpp -o main_2 -O3 -Wall -pedantic -DMMX -std=c++17 > /dev/null
 ./main_2 example
 
 
 echo 'SSE:'
-g++ main.cpp -o main_3 -O0 -Wall -pedantic -DSSE -g3 > /dev/null
+g++ main.cpp -o main_3 -O3 -Wall -pedantic -DSSE -std=c++17 > /dev/null
 ./main_3 example_float
